fc994db0cc7978a97f24303595ad8c6f415aa22d
lilfaithontrack <kalu4mom@gmail.com>
2026-04-13 07:49:54 +0300

    new cors link added

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.use((req, res, next) => {
 // Middleware
 const allowedProductionOrigins = [
       'https://shegergebeya.com',
-    'https://admin.shegergebeya.com',
+    'https://admin.shegergebeya.com','http://localhost:5173'
 ];
 
 app.use(cors({
